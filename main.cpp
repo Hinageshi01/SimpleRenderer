ce595a7d7a09055427276d7b57e583f57740b0c8
Hinageshi01 <2247001665@qq.com>
2021-11-08 12:06:16 +0800

    添加主循环

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 #undef max
 #undef min
 
-int fun() {
-    return -1;
-}
-
 int main() {
     // 准备模型数据。
     Model* model = nullptr;
@@ -16,12 +12,11 @@ int main() {
     //model = new Model("obj/diablo3_pose/diablo3_pose.obj");
 
     // 准备顶点着色器。
-    // TODO：物体会旋转，相机会移动，这个晚点要写到主循环里面
-    float angleY = 20.f;
+    float angleY = 0.f;
     float scale = 3.5f;
     Eigen::Vector3f move = {0,0.1f,0};
     Eigen::Vector3f eyePos = {0,0,10};
-    Frustum fru = {45.f, 1.f, 0.1f, 50.f};
+    Frustum fru = {45.f, WIDTH / (float)HEIGHT, 0.1f, 50.f};
     VertexShader vs(angleY, scale, move, eyePos, fru);
 
     // 准备光栅化器。
@@ -30,77 +25,75 @@ int main() {
     
     // 准备深度缓冲。
     float* z_buffer = new float[WIDTH * HEIGHT];
-    memset(z_buffer, std::numeric_limits<float>::max(), WIDTH * HEIGHT * sizeof(float));
 
+    ExMessage msg;
+    float sumTime = 0;
+    int sumFrame = 0;
     initgraph(WIDTH, HEIGHT);
 
-    float startTime = omp_get_wtime();
-#pragma omp parallel for
-    for (int i = 0; i < model->nfaces(); i++) {
-        // 这个循环里遍历所有三角面。
-        std::vector<int> face = model->face(i);
-
-        // 装配三角形。
-        Vertex vertex[3];
-        for (int j = 0; j < 3; j++) {
-            Eigen::Vector3f tmpP = model->vert(face[j]);
-            vertex[j].pos = Eigen::Vector4f(tmpP[0], tmpP[1], tmpP[2], 1.f);
-
-            Eigen::Vector3f tmpN = model->normal(i, j);
-            vertex[j].normal = Eigen::Vector4f(tmpN[0], tmpN[1], tmpN[2], 0.f);
-
-            vertex[j].uv = model->uv(i, j);
+    // 主循环。
+    while (true) {
+        // 处理键盘输入。
+        msg = getmessage(EM_KEY);
+        if (msg.message == WM_KEYDOWN) {
+            BYTE code = msg.vkcode;
+            if (code == VK_ESCAPE || code == 0x0D) {
+                closegraph();
+                break;
+            }
+            switch (code) {
+                case 0x41:
+                    angleY += 3;
+                    break;
+                case 0x44:
+                    angleY -= 3;
+                    break;
+                case 0x53:
+                    scale -= 0.3;
+                    break;
+                case 0x57:
+                    scale += 0.3;
+                    break;
+            }
         }
+        vs.Update(angleY, scale);
 
-        // Vertex Shader
-        vs.Transform(vertex);
+        cleardevice();
+        BeginBatchDraw();
+        memset(z_buffer, std::numeric_limits<float>::max(), WIDTH * HEIGHT * sizeof(float));
 
-        // 光栅化与 Fragement Shader
-        r.RasterizeTriangle_SL(vertex, model, z_buffer);
+        float startTime = omp_get_wtime();
+    #pragma omp parallel for
+        for (int i = 0; i < model->nfaces(); i++) {
+            // 这个循环里遍历所有三角面。
+            std::vector<int> face = model->face(i);
+
+            // 装配三角形。
+            Vertex vertex[3];
+            for (int j = 0; j < 3; j++) {
+                Eigen::Vector3f tmpP = model->vert(face[j]);
+                vertex[j].pos = Eigen::Vector4f(tmpP[0], tmpP[1], tmpP[2], 1.f);
+
+                Eigen::Vector3f tmpN = model->normal(i, j);
+                vertex[j].normal = Eigen::Vector4f(tmpN[0], tmpN[1], tmpN[2], 0.f);
+
+                vertex[j].uv = model->uv(i, j);
+            }
+
+            // 顶点着色器。
+            vs.Transform(vertex);
+
+            // 光栅化与片元着色器。
+            r.RasterizeTriangle_SL(vertex, model, z_buffer);
+        }
+        float endTime = omp_get_wtime();
+        sumTime += (endTime - startTime);
+        sumFrame++;
+
+        FlushBatchDraw();
     }
-    float endTime = omp_get_wtime();
-    std::cout << "Rendered 1 frame with " << endTime - startTime << " s" << std::endl;
-
-    _getch();
+    EndBatchDraw();
+    std::cout << "Rendering 1 frame with averge " << sumTime / (float)sumFrame << " s" << std::endl;
 
     return 0;
 }
-
-// 调试起来太别扭了，最后再考虑把管线放入主循环
-//int main() {
-//    ExMessage m;
-//
-//    float sumTime = 0;
-//    int sumFrame = 0;
-//    BeginBatchDraw();
-//    while (true) {
-//        m = getmessage(EM_KEY);
-//        if (m.message == WM_KEYDOWN) {
-//            if (m.vkcode == VK_ESCAPE) {
-//                closegraph();
-//                break;
-//            }
-//            if (m.vkcode == 0x41) {
-//                angle++;
-//            }
-//            if (m.vkcode == 0x44) {
-//                angle--;
-//            }
-//        }
-//        cleardevice();
-//        memset(z_buffer, std::numeric_limits<float>::max(), WIDTH * HEIGHT * sizeof(float));
-//        float startTime = omp_get_wtime();
-//    #pragma omp parallel for
-//        for (int i = 0; i < model->nfaces(); i++) {
-//            // 这个循环里遍历所有三角面。
-//        }
-//        float endTime = omp_get_wtime();
-//        sumTime += (endTime - startTime);
-//        sumFrame++;
-//        FlushBatchDraw();
-//    }
-//    EndBatchDraw();
-//    std::cout << "Rendering 1 frame with averge " << sumTime / (float)sumFrame << " s" << std::endl;
-//
-//	return 0;
-//}
