cafcdcd2b69d517222fa361af5149ae4616194ef
Hinageshi01 <2247001665@qq.com>
2021-11-11 11:46:09 +0800

    代码优化

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -140,13 +140,11 @@ void Rasterizer::RasterizeTriangle_SL(Vertex* v,  Model* const model, float* z_b
                 Eigen::Vector3f viewPos = Interpolate(a, b, c, v[0].viewPos, v[1].viewPos, v[2].viewPos);
                 Eigen::Vector4f tmpN = Interpolate(a, b, c, v[0].normal, v[1].normal, v[2].normal);
 
-                // 重要的三个向量，均以着色点为出发点。
+                // 重要的几个向量，均以着色点为出发点。
                 Eigen::Vector3f normal = tmpN.head<3>().normalized();
                 Eigen::Vector3f viewDir = (eyePos - viewPos).normalized();
                 Eigen::Vector3f lightDir = (light.position - viewPos).normalized();
-
-                Eigen::Vector3f VL = viewDir + lightDir;
-                Eigen::Vector3f halfDir = ((VL) / (VL).dot(VL)).normalized();
+                Eigen::Vector3f halfDir = (viewDir + lightDir).normalized();
 
                 // 对 uv 插值。
                 Eigen::Vector2f uv = Interpolate(a, b, c, v[0].uv, v[1].uv, v[2].uv);
@@ -198,86 +196,3 @@ void Rasterizer::RasterizeTriangle_SL(Vertex* v,  Model* const model, float* z_b
 inline void Rasterizer::WorldToScreen(Eigen::Vector4f& v) {
     v = Eigen::Vector4f(int((v[0] + 1.) * WIDTH / 2. + 0.5), HEIGHT - int((v[1] + 1.) * HEIGHT / 2. + 0.5), v[2], 1.f);
 }
-
-//inline bool Rasterizer::InsideTriangle(const Eigen::Vector4f* v, const Eigen::Vector3f& p) {
-//    Eigen::Vector3f a(v[0][0], v[0][1], 0);
-//    Eigen::Vector3f b(v[1][0], v[1][1], 0);
-//    Eigen::Vector3f c(v[2][0], v[2][1], 0);
-//
-//    Eigen::Vector3f ab = b - a;
-//    Eigen::Vector3f bc = c - b;
-//    Eigen::Vector3f ca = a - c;
-//
-//    Eigen::Vector3f ap = p - a;
-//    Eigen::Vector3f bp = p - b;
-//    Eigen::Vector3f cp = p - c;
-//
-//    Eigen::Vector3f crs1 = ab.cross(ap);
-//    Eigen::Vector3f crs2 = bc.cross(bp);
-//    Eigen::Vector3f crs3 = ca.cross(cp);
-//
-//    return ((crs1[2] >= 0 && crs2[2] >= 0 && crs3[2] >= 0) ||
-//            (crs1[2] <= 0 && crs2[2] <= 0 && crs3[2] <= 0));
-//}
-//
-//void Rasterizer::RasterizeTriangle_AABB(Eigen::Vector4f* v, Eigen::Vector4f* n, float* z_bufer) {
-//    Eigen::Vector3f a(v[0][0], v[0][1], 0.f);
-//    Eigen::Vector3f b(v[1][0], v[1][1], 0.f);
-//    Eigen::Vector3f c(v[2][0], v[2][1], 0.f);
-//
-//    Eigen::Vector3f ab = b - a;
-//    Eigen::Vector3f bc = c - b;
-//    Eigen::Vector3f faceNormal = ab.cross(bc);
-//    if (faceNormal[2] < 0) {
-//        //TODO：这里只关心z分量，优化一下矩阵运算
-//        // 当三角面背对视点时，不做光栅化。
-//        return;
-//    }
-//
-//    if ((v[0][1] == v[1][1] && v[0][1] == v[2][1]) || (v[0][0] == v[1][0] && v[0][0] == v[2][0])) {
-//        // 三角面完全侧对视点时，不做光栅化。
-//        return;
-//    }
-//
-//    int min_x = (int)std::floor(std::min(v[0][0], std::min(v[1][0], v[2][0])));
-//    int max_x = (int)std::ceil(std::max(v[0][0], std::max(v[1][0], v[2][0])));
-//    int min_y = (int)std::floor(std::min(v[0][1], std::min(v[1][1], v[2][1])));
-//    int max_y = (int)std::ceil(std::max(v[0][1], std::max(v[1][1], v[2][1])));
-//
-//    min_x = std::clamp(min_x, 0, WIDTH - 1);
-//    max_x = std::clamp(max_x, 0, WIDTH - 1);
-//    min_y = std::clamp(min_y, 0, HEIGHT - 1);
-//    max_y = std::clamp(max_y, 0, HEIGHT - 1);
-//
-//    for (int x = min_x; x <= max_x; x++) {
-//        for (int y = min_y; y <= max_y; y++) {
-//            if (InsideTriangle(v, Eigen::Vector3f(x, y, 0))) {
-//                int index = x + y * WIDTH;
-//
-//                Eigen::Vector3f tmpBC = BarycentricCoor(x + 0.5f, y + 0.5f, v);
-//                float a = tmpBC[0];
-//                float b = tmpBC[1];
-//                float c = tmpBC[2];
-//
-//                float a_div_w = a / (float)v[0][3];
-//                float b_div_w = b / (float)v[1][3];
-//                float c_div_w = c / (float)v[2][3];
-//                float z = 1.0f / (a_div_w + b_div_w + c_div_w);
-//                float zp = a_div_w * v[0][2] + b_div_w * v[1][2] + c_div_w * v[2][2];
-//                zp *= z;
-//
-//                if (zp <= z_bufer[index]) {
-//                    z_bufer[index] = zp;
-//
-//                    Eigen::Vector4f normal = Interpolate(a, b, c, n[0], n[1], n[2]);
-//                    float cosTerm = std::max(0.f, normal.head<3>().normalized().dot(light.direction.normalized()));
-//                    cosTerm += 0.1f;
-//                    cosTerm = std::clamp(cosTerm, 0.0f, 1.0f);
-//                    COLORREF color = RGB(cosTerm * light.intensity[0], cosTerm * light.intensity[1], cosTerm * light.intensity[2]);
-//
-//                    putpixel(x, y, color);
-//                }
-//            }
-//        }
-//    }
-//}
