5b439e75251c6388a61d2b9e77c3feaff1e7f5d9
Hinageshi01 <2247001665@qq.com>
2022-03-20 18:03:57 +0800

    结构优化

--- vertexShader.cpp
+++ vertexShader.cpp
@@ -4,12 +4,12 @@ VertexShader::VertexShader(const float &a, const float &s, const Eigen::Vector3f
     angleY(a), scale(s), move(m), fru(f)
 {
     float radian = fru.fov * MY_PI / 180.f;
-    float t = tan(radian / 2) * abs(fru.zNear);
+    float t = tan(radian / 2.f) * abs(fru.zNear);
     float b = -t;
     float r = fru.aspect * t;
     float l = -r;
 
-    // 计算 projection 矩阵。
+    // 计算 projection 矩阵
     Eigen::Matrix4f Mpo;
     Mpo << fru.zNear, 0, 0, 0,
         0, fru.zNear, 0, 0,
@@ -22,13 +22,13 @@ VertexShader::VertexShader(const float &a, const float &s, const Eigen::Vector3f
         0, 0, 0, 1;
     projection = Mo * Mpo;
 
-    // 计算 view 矩阵。
+    // 计算 view 矩阵
     view << 1, 0, 0, -e[0],
         0, 1, 0, -e[1],
         0, 0, 1, -e[2],
         0, 0, 0, 1;
 
-    // 计算 model 矩阵中移动的部分。
+    // 计算 model 矩阵中移动的部分
     Mmove << 1, 0, 0, move[0],
         0, 1, 0, move[1],
         0, 0, 1, move[2],
@@ -60,33 +60,33 @@ Eigen::Matrix4f VertexShader::GetModelMatrix() {
 void VertexShader::Transform(Vertex *vertex) {
     Eigen::Matrix4f model = GetModelMatrix();
 
-    // 用于保留 viewSpace 坐标系。
+    // 用于保留 viewSpace 坐标系
     Eigen::Matrix4f modelView = view * model;
-    // mvp 矩阵。
+    // mvp 矩阵
     Eigen::Matrix4f mvp = projection * modelView;
-    // mv 矩阵的逆的转置，用于处理顶点法向量。
+    // mv 矩阵的逆的转置，用于处理顶点法向量
     Eigen::Matrix4f modelViewInvTrans = modelView.inverse().transpose();
 
     for (int i = 0; i < 3; ++i) {
-        // 给片元着色器提供用于计算光照的线性空间坐标。
+        // 给片元着色器提供用于计算光照的线性空间坐标
         vertex[i].viewPos = (modelView * vertex[i].pos).head<3>();
 
         Eigen::Vector4f p = mvp * vertex[i].pos;
 
-        // 归一化至 NDC 坐标系。
+        // 归一化至 NDC 坐标系
         p[3] = 1.f / p[3];
         p[0] *= p[3];
         p[1] *= p[3];
         p[2] *= p[3];
 
-        // 视口变换。
+        // 视口变换
         p[0] = float(0.5f * WIDTH * (p[0] + 1.f));
         p[1] = float(0.5f * HEIGHT * (p[1] + 1.f));
         //p[2] *= (fru.zFar - fru.zNear) / 2.f + (fru.zFar + fru.zNear) / 2.f;
 
         vertex[i].pos = p;
 
-        // 法向量变换。
+        // 法向量变换
         vertex[i].normal = modelViewInvTrans * vertex[i].normal;
     }
 }
--- vertexShader.h
+++ vertexShader.h
@@ -1,9 +1,20 @@
 #pragma once
 
-#include "vertex.h"
+#include "global.h"
 
 #undef near
 #undef far
+
+class Vertex
+{
+public:
+	Eigen::Vector4f pos;
+	Eigen::Vector4f normal;
+	Eigen::Vector2f uv;
+
+	// viewSpace 坐标系下的顶点坐标。
+	Eigen::Vector3f viewPos;
+};
 
 class VertexShader
 {
